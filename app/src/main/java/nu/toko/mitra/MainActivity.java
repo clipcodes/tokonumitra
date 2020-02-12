a4d3cd37112d1f0e574fd1fed8dc4a161f3b4955
dwilanang <dwi@samdesign.com>
2020-02-12 10:33:01 +0700

    Tes

--- app/src/main/java/nu/toko/mitra/MainActivity.java
+++ app/src/main/java/nu/toko/mitra/MainActivity.java
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 //        FirebaseMessaging.getInstance().subscribeToTopic("oke");
         init();
         declare();
+
+        //Oke
     }
 
     public void init(){
